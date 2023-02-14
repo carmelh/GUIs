43b1557125c3684ae21ebcdcd721ede417d11338
Carmel <33908428+carmelh@users.noreply.github.com>
2023-02-13 16:46:13 -0800

    finished folder gen starting analysis GUI

--- ephysAnalysis_OHSU/ephysAnalysis_header.py
+++ ephysAnalysis_OHSU/ephysAnalysis_header.py
@@ -11,7 +11,7 @@ import ephys_analysis_funcs as ef
 from PyQt5 import QtCore, QtGui, uic, QtWidgets
 
 
-qtCreatorFile = r'C:\Users\howeca\Documents\GitHub\GUIs\ephysAnalysis_OHSU\folderGenerator.ui' # Enter file here.
+qtCreatorFile = r'C:\Users\howeca\Documents\GitHub\GUIs\ephysAnalysis_OHSU\runAnalysis.ui' # Enter file here.
  
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
  
@@ -21,7 +21,6 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         Ui_MainWindow.__init__(self)
         self.setupUi(self)
         self.browseButton.clicked.connect(self.getFolderPath)
-        self.makeFoldersButton.clicked.connect(self.generateFolders)
         self.closeButton.clicked.connect(self.closeEvent)
 
 #  user selects parent folder, i.e. the experiment day
@@ -29,22 +28,6 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select Folder')
         self.pathTextBox.setText(folder_path)
         return folder_path
-
-# generates the folders within the parent folder selected by user
-    def generateFolders(self): 
-        folder_path = self.pathTextBox.toPlainText()
-        if self.expComboBox.currentText() == "Current Clamp":
-            self.printNoteBox.setText("Current clamp")
-            expParam = "cClamp"
-            ef.makeFoldersAnalysis(folder_path,expParam)
-        elif self.expComboBox.currentText() == "Voltage Clamp":
-            self.printNoteBox.setText("Voltage clamp")
-            expParam = "vClamp"
-            ef.makeFoldersAnalysis(folder_path,expParam)
-        else: 
-            self.printNoteBox.setText("Select an experiment")   
-        
-        self.printNoteBox_2.setText("Go organise trials into folders then open analysis GUI")
 
     def closeEvent(self):
         self.close()
